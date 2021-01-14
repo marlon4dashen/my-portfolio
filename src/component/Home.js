6aa4ded00d88a588fb001864c80a430efcc3f1ce
marlon4dashen <alienlql@hotmail.com>
2021-01-14 01:38:47 -0500

    change alt

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,7 +6,7 @@ export default function Home(){
     return (
 
         <main>
-            <img src={image} alt="Background Image" className="absolute object-cover w-full h-full"
+            <img src={image} alt="backgroundimg" className="absolute object-cover w-full h-full"
             />
             <section className="relative flex justify-center min-h-screen pt12 lg:pt-64 px 8">
                 <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hola, I am Marlon</h1>
