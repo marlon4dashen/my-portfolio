cf082fc2f885cdb9b4c56fe1b5a66724a3fd56e4
Marlon Liu <alienlql@hotmail.com>
2022-09-21 22:43:51 -0400

    remove about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./component/Home";
-import About from "./component/About";
 import SinglePost from "./component/SinglePost";
 import Post from "./component/Post";
 import Project from "./component/Project";
@@ -13,8 +12,6 @@ function App() {
     {/* <NavBar/> */}
       <Switch>
         <Route component={Home} path = '/' exact />
-        <Route component={About} path = '/about' />
-        {/* <Route component={SinglePost} path = '/post/:slug' /> */}
         <Route component={Post} path = '/post' />
         <Route component={Project} path = '/project' />
       </Switch>
